547b8ccba90e8d80d4620d8f11c90d59eb0362ee
Jackson <ja_amirt@live.concordia.ca>
2024-03-10 11:45:09 -0400

    models

--- frontvue/src/App.vue
+++ frontvue/src/App.vue
@@ -1,26 +1,15 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>App</strong></router-link>
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbar-menu" class="navbar-menu">
-        <div class="navbar-end">
-          <router-link to="/about" class="navbar-item">About</router-link>
-          <router-link to="/contact" class="navbar-item">Contact</router-link>
-        </div>
-      </div>
-    </nav>
-  </div>
-
-
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">Home</router-link>
+      <router-link to="/about" class="navbar-item">About</router-link>
+    </div>
+    
+  </nav>
+  <router-view/>
+</div>
 </template>
-
 
 <style lang="scss">
 @import '../node_modules/bulma';
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,17 +1,12 @@
+
 from django.db import models
 
-# Create your models here.
-
-
 class Car(models.Model):
-    make = models.CharField(max_length=50, default='unknown')
-    model = models.CharField(max_length=50, default='unknown')
-    year = models.PositiveIntegerField(default=0000)
-    price = models.PositiveIntegerField(default=0)
-    reserved = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='car_images/', default='car_images/default.jpg')
-    mileage = models.PositiveIntegerField(default=0)
-    company_uuid = models.UUIDField(null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.year} {self.make} {self.model} - ${self.price} - Mileage: {self.mileage} miles"
+    id = models.AutoField(primary_key=True)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='car_images')
+    is_electric = models.BooleanField(default=False)
+    is_all_wheel_drive = models.BooleanField(default=False)
