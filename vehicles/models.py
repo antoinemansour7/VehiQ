21875c3cabf316c6d7c79a487f50262cdfc79b6f
Jackson <ja_amirt@live.concordia.ca>
2024-03-10 12:20:06 -0400

    models

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,12 +1,22 @@
-
+from io import BytesIO
+from PIL import Image
 from django.db import models
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
-    make = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    year = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='car_images')
+    make = models.CharField(max_length=100, default='')
+    model = models.CharField(max_length=100, default='')
+    year = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     is_electric = models.BooleanField(default=False)
     is_all_wheel_drive = models.BooleanField(default=False)
+    
+    def _str_(self):
+        return f'{self.year} {self.make} {self.model}'
+    
+    def get_image(self):
+        if self.image:
+             return 'http://127.0.0.1:800'+self.image.url
+        return ''
+
