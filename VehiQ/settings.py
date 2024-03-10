fedf9827b260dd1d5baaf0da681ce5fe466857ee
Jackson <ja_amirt@live.concordia.ca>
2024-03-10 03:41:05 -0400

    djoser

--- VehiQ/settings.py
+++ VehiQ/settings.py
@@ -48,10 +48,14 @@ INSTALLED_APPS = [
     
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
