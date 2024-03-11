c0f77a5f96a40be5a9137a1edef7a6540e1d8c18
andre <ggandreassaad@gmail.com>
2024-03-11 11:36:50 -0400

    feat(accounts/forms.py,-models.py,-urls.py,-views.py,-and-register.html): added register functionality
    added views, urls, models, forms, and html to enable users/companies to register

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,11 @@
+import string
 from django import forms
 from .models import Profile, Reservation
 from django.core.validators import RegexValidator
 from django.forms import ValidationError
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
 
 class ProfileForm(forms.ModelForm):
     phone = forms.CharField(validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")])
@@ -21,3 +24,33 @@ class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['car', 'start_date', 'end_date',] 
+
+
+def validate_no_special_characters(value):
+    if any(char not in string.ascii_letters + string.digits for char in value):
+        raise ValidationError("Special characters are not allowed.")
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    user_type = forms.ChoiceField(choices=Profile.USER_TYPE_CHOICES, widget=forms.RadioSelect)
+    username = forms.CharField(
+        max_length=Profile._meta.get_field('user').max_length,
+        help_text="Username must not contain special characters!",
+        validators=[validate_no_special_characters]
+    )
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+        profile.email = self.cleaned_data['email']
+        profile.save()
+
+        user_type = self.cleaned_data['user_type']
+        profile.user_type = user_type
+        if commit:
+            profile.save()
+
+        return profile
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
@@ -11,11 +12,20 @@ class Profile(models.Model):
         ('private', 'Private'), 
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    USER_TYPE_CHOICES = [
+        ('individual', 'Individual'),
+        ('company', 'Company'),
+    ]    
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='individual')
     # Example additional fields
     bio = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     visibility = models.CharField(max_length=7, choices=VISIBILITY_CHOICES, default='public')
+
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    company_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.user.username
