a80e7c1babffdcf7d14d66089a07a28d4a4fd7f9
l3itisys <leiticia.tlb@gmail.com>
2024-03-15 14:30:54 -0400

    Fix Flake8 error

--- branches/admin.py
+++ branches/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from .models import Branch
 
+
 @admin.register(Branch)
 class BranchAdmin(OSMGeoAdmin):
     list_display = ('name', 'location', 'address', 'city')
