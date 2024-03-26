c4fec815cd2103404eafef108ddda53c72cce43d
l3itisys <leiticia.tlb@gmail.com>
2024-03-25 22:21:17 -0400

    Updated urls.py

--- branches/urls.py
+++ branches/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from . import views
+import pprint
 
 router = SimpleRouter()
 router.register(r'branches', views.BranchViewSet)
@@ -10,3 +11,5 @@ urlpatterns = [
     path('geocode/', views.get_geocode, name='get_geocode'),
     path('nearest-branch', views.get_nearest_branch, name='get_nearest_branch'),
 ]
+
+pprint.pprint(router.urls)
