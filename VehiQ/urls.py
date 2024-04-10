f406652b91a13e462f09a8b20d98942d07e15efa
j-ackamir <j.ackson@live.ca>
2024-04-10 19:48:01 -0400

    image urls

--- VehiQ/urls.py
+++ VehiQ/urls.py
@@ -1,12 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')), 
-    path('api/v1/', include('vehicles.urls')), 
-    path('vehicles/', include('vehicles.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('api/v1/', include('djoser.urls')), # new
+    path('api/v1/', include('djoser.urls.authtoken')), # new
+    path('api/v1/', include('vehicles.urls')), # new
+    path('vehicles/', include('vehicles.urls')), 
     path('accounts/', include('accounts.urls')),
-    path('branches/', include('branches.urls')),]
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
